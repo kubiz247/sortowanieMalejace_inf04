9374c0ad977153a59c76190d54da994facc5481f
Jakub <uT_3773@zsp3zamosc.pl>
2025-10-15 09:54:49 +0200

    Dodaj pliki projektów.

---
+++ sortowanieMalejace_inf04/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sortowanieMalejace_inf04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 50;
+            if(args.Length > 0 && int.TryParse(args[0],out int parsed) &&  parsed > 0)
+            {
+                n = parsed;
+            }
+            int[] tab = new int[n];
+            Random rand = new Random();
+
+            for(int i = 0; i < n; i++)
+            {
+                tab[i] = rand.Next(0, 101);
+            }
+
+            Console.WriteLine($"Tablica ({n} elementow): ");
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write($"{i}: {tab[i]}");
+            }
+        }
+    }
+}
